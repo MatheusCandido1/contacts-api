9ec50e66393c4dd3f7ad09cfba4597ad43fd57a7
matheus <matheus12bh@gmail.com>
2022-01-16 11:27:54 -0800

    add-multiple-env-config

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,9 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
+
 app.use(cors());
 app.use(routes);
 app.use(error);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function () { ('Server running at: http://localhost:3000')});
