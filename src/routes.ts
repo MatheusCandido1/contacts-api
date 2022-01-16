0935b86a9f877c173b83fafdea778526adc40126
matheus <matheus12bh@gmail.com>
2022-01-16 15:38:09 -0800

    update-title

--- src/routes.ts
+++ src/routes.ts
@@ -6,11 +6,15 @@ import swaggerUi from "swagger-ui-express";
 
 const swaggerDocument  = require('./swagger');
 
+var swaggerOptions = {
+  customSiteTitle: "MyContacts API - Swagger Docs"
+}
+
 const routes = Router();
 
 //Swagger
 routes.use('/', swaggerUi.serve);
-routes.get('/', swaggerUi.setup(swaggerDocument));
+routes.get('/', swaggerUi.setup(swaggerDocument, swaggerOptions));
 
 // API V1
 routes.use('/api/v1', apiv1);
