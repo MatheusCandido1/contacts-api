c8f1349494cf91f23e199c5af070286bd0db18d6
matheus <matheus12bh@gmail.com>
2022-01-15 16:24:46 -0800

    re-deploy

--- dist/middlewares/error.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (error, request, response, next) => {
-    return response.sendStatus(500);
-};
-exports.default = errorHandler;
